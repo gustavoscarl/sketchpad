4bae14d045dd2b7b89b5c0fbaa853d8b13092fa5
Gustavo Scarcelli Lucianelli <104444836+gustavoscarl@users.noreply.github.com>
2023-07-03 19:04:51 -0300

    added create grid function and refactored code

--- script.js
+++ script.js
@@ -1,18 +1,67 @@
 const container = document.querySelector('.mainContainer')
+let counter = 0;
+let counterDo = 0;
+let gridSize = 16;
 
-for (let i = 0; i <= 255; i++ ) {
-  const div = document.createElement('div')
-  div.classList.add('gridSection')
-  container.appendChild(div)
+  for (let i = 0; i <= 255; i++ ) {
+    const div = document.createElement('div')
+    div.classList.add('gridSection')
+    container.appendChild(div)
+  };
+
+
+
+
+
+
+
+
+// Button for newGrid
+
+const button = document.querySelector('#newGrid')
+
+button.addEventListener('click', function (){
+  gridSection.forEach(function (item) {
+    removeCells()
+  });
+  counter++;
+  gridSize = window.prompt('Escolha o tamanho do seu grid (máximo: 100', 0 );
+  createGrid();
+})
+
+function removeCells() {
+  while(container.firstChild) {
+    container.removeChild(container.firstChild);
+  }
 }
 
-// Add color when hover .gridSection
 
-const gridSection = document.querySelectorAll('.gridSection')
+function createGrid () {
+    let gridWidth = container.offsetWidth / gridSize;
+    container.style.gridTemplateColumns = `repeat(${gridSize}, ${gridWidth}px) 1fr`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, ${gridWidth}px) 1fr 1fr 1fr`;
+  if (gridSize < 4) {
+    container.style.gridTemplateColumns = `repeat(${gridSize},1fr`;
+    container.style.gridTemplateRows = `repeat(${gridSize}, 1fr`;
+  }
+  for (let i = 0; i < gridSize ** 2; i++) {
+    const square = document.createElement('div');
+    square.classList.add('gridSection');
+    container.appendChild(square); 
+  }
+  gridSection = document.querySelectorAll('.gridSection');
+  addColorToSquares();
+};
 
+// Add color to existing grid sections
 
-gridSection.forEach (function (item, index) {
-  item.addEventListener('mouseover', function addColor () {
-      item.classList.add('active')
-  })
-});
+function addColorToSquares() {
+  gridSection.forEach(function (item) {
+    item.addEventListener('mouseover', function () {
+      item.classList.add('active');
+    });
+  });
+}
+
+createGrid();
+
